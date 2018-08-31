3ce45b792f3daf28c37bd84577b925695e254e58
System Administrator <root@MacBook-Air-de-xiencias.local>
2018-08-31 11:35:39 -0500

    cambios

--- capital/urls.py
+++ capital/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import  include, url
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from app.views import *
@@ -15,20 +15,20 @@ urlpatterns = [
     # url(r'^$', 'views.IndexView'),
     url(r'^admin/', admin.site.urls),
     
-    url(r'^api-token-auth/', 'jwt_auth.views.obtain_jwt_token'),
+    # url(r'^api-token-auth/', 'jwt_auth.views.obtain_jwt_token'),
     url(r'^pos/', admin_site.urls),
     url(r'api-token-refresh/', refresh_jwt_token),
-    url(r'^login/$', 'app.views.login2'),
-    url(r'^subir/$', 'app.views.subir'),
-    url(r'^masacre/$', 'app.views.masacre'),
+    url(r'^login/$', login2),
+    url(r'^subir/$', subir),
+    url(r'^masacre/$', masacre),
 
 
-    url(r'^ingresar/$','app.views.ingresar'),
-    url(r'^dashboard/$', 'app.views.dashboard'),
-    url(r'^album/$', 'app.views.album'),
+    url(r'^ingresar/$',ingresar),
+    url(r'^dashboard/$', dashboard),
+    url(r'^album/$', album),
 
-    url(r'^paciente/$', 'app.views.paciente'),
-    url(r'^nuevopaciente/$', 'app.views.nuevopaciente'),
+    url(r'^paciente/$', paciente),
+    url(r'^nuevopaciente/$', nuevopaciente),
 
     url(r'^agente/$', Agenterest.as_view()),
     url(r'^userfono/$', Userfono.as_view()),
@@ -81,10 +81,10 @@ urlpatterns = [
     url(r'^sacareportepropuestas/$', Sacareportepropuestas.as_view()),
     url(r'^sacareportepos/$', Sacareportepos.as_view()),
     url(r'^semanasresumen/$', Semanasresumen.as_view()),
-    url(r'^subirarchivo/$', 'app.views.subirarchivo'),
-    url(r'^subire/$', 'app.views.subire'),
-    url(r'^asignanotificacion/$', 'app.views.asignanotificacion'),
-    url(r'^guardar$', 'app.views.guardar'),
+    url(r'^subirarchivo/$', subirarchivo),
+    url(r'^subire/$', subire),
+    url(r'^asignanotificacion/$', asignanotificacion),
+    url(r'^guardar$', guardar),
     
     #url(r'^enviasms$', Enviasms.as_view()),
 
